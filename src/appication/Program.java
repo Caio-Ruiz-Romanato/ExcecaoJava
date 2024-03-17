fe16bd0fd80226fd88c5c2d3c393915504c2a166
Caio-Ruiz-Romanato <caioruizromanato@gmail.com>
2024-03-17 12:44:09 -0300

    Solucao Ruim

--- src/appication/Program.java
+++ src/appication/Program.java
@@ -12,51 +12,42 @@ public class Program {
 
 	public static void main(String[] args) throws ParseException {
 		// TODO Auto-generated method stub
-		
+
 		Scanner sc = new Scanner(System.in);
 		Locale.setDefault(Locale.US);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-		
+
 		System.out.print("Room number: ");
 		int number = sc.nextInt();
 		System.out.print("Check-In date (dd/MM/yyyy) ");
 		Date checkIn = sdf.parse(sc.next());
 		System.out.print("Check-Out date (dd/MM/yyyy) ");
 		Date checkOut = sdf.parse(sc.next());
-		
-		if(!checkOut.after(checkIn) ) {
+
+		if (!checkOut.after(checkIn)) {
 			System.out.println("Error in Reservation: check-out date must be after check-in date");
-		}
-		else {
+		} else {
 			Reservation reservation = new Reservation(number, checkIn, checkOut);
 			System.out.println("Reservation" + reservation);
-			
+
 			System.out.println();
 			System.out.println("Enter date to update the reservation: ");
 			System.out.print("Check-In date (dd/MM/yyyy) ");
 			checkIn = sdf.parse(sc.next());
 			System.out.print("Check-Out date (dd/MM/yyyy) ");
 			checkOut = sdf.parse(sc.next());
-			
-			//Solucao de contorno ruim
-			Date now  = new Date();
-			if(checkIn.before(now) || checkOut.before(now)) {
-				System.out.println("Error in reservation: Reservation dates for updates must be future dates. ");
-				
-			}
-			else if(!checkOut.after(checkIn) ){
-				System.out.println("Error in reservation: Reservation dates for updates must be future dates. ");
-			}
-			else {
-				reservation.updateDates(checkIn, checkOut);
+
+			String error = reservation.updateDates(checkIn, checkOut);
+			if (error != null) {
+				System.out.println("Error in reservation: " + error);
+
+			} else {
 				System.out.println("Reservation" + reservation);
 			}
-			
+
 		}
-		
+
 		sc.close();
 	}
 
 }
-
-
--- src/model/entities/Reservation.java
+++ src/model/entities/Reservation.java
@@ -41,9 +41,18 @@ public class Reservation {
 	}
 
 	// atualiza checkIn e checkOut
-	public void updateDates(Date checkIn, Date checkOut) {
+	public String updateDates(Date checkIn, Date checkOut) {
+		// Solucao de contorno ruim
+		Date now = new Date();
+		if (checkIn.before(now) || checkOut.before(now)) {
+			return "Error in reservation: Reservation dates for updates must be future dates. ";
+
+		} if (!checkOut.after(checkIn)) {
+			return "Error in reservation: Reservation dates for updates must be future dates. ";
+		}
 		this.checkIn = checkIn;
 		this.checkOut = checkOut;
+		return null;
 	}
 
 	@Override
